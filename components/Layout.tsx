5187d9db4a7905de5631de0389e82fe69a6ec056
shiroemons <shiroemons@gmail.com>
2020-10-31 19:09:45 +0900

    Add tailwind settings

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,7 +8,7 @@ type Props = {
 }
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
+  <div className="bg-gray-100">
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
